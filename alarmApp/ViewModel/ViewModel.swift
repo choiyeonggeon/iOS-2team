0ca2e05953f6d2ddd6163784cbc1815efd0731a9
choiyeonggeon <aws8792@naver.com>
2025-05-20 11:12:44 +0900

    2team

---
+++ alarmApp/ViewModel/ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModel.swift
+//  alarmApp
+//
+//  Created by 최영건 on 5/20/25.
+//
+
+import Foundation
---
+++ alarmApp/alarmApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  alarmApp
+//
+//  Created by 최영건 on 5/20/25.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
